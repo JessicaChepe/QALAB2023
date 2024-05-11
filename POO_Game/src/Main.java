3a1f04bceab37d0f1f8089f3e8b4c273240d8ad3
jessi_che <jessicachepepaniagua@gmail.com>
2024-05-10 21:38:59 -0500

    Agredando comentario en Main Linea 3

--- POO_Game/src/Main.java
+++ POO_Game/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        // "editado por Jessica Chepe"
 
         Toyota toyo = new Toyota("PAS201"); //200
         toyo.encenderMotor();
